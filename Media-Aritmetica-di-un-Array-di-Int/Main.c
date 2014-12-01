56c9b64d74929d13189849b23102bd31eb1f2975
Giancarmine <giancarmine.cuofano@gmail.com>
2014-12-01 19:23:28 +0100

    Create Main.c

---
+++ Media-Aritmetica-di-un-Array-di-Int/Main.c
@@ -0,0 +1,50 @@
+#include "stdio.h"
+
+int MediaAritmetica(int Array[], int N);
+
+/*==============================================================================
+
+
+Autore      : Carmine Cuofano		Matricola: N86001700
+Programma   : Media di un vettore di int
+Data        : 01/12/2014
+
+To Do:
+
+------------------------------------------------------------------------------*/
+
+
+main ()
+{
+    //Dichiarazione
+    int     Array [100];
+    int     i,
+            N,
+            Media;
+
+    //Inizializzazione del array da ordinare
+    printf ("Inserisci il numero di elementi del array:");
+    scanf ("%d",&N);
+
+    for (i=0; i<N; i++){
+        printf("\nInserisci il valore della cella[%d]", i);
+        scanf("%d",&Array[i]);
+    }
+    //Chiamata delle function
+    Media = MediaAritmetica( Array, N);
+    //Stampo del risultato
+    printf("\nIl risultato e:%d",Media);
+}
+
+
+int MediaAritmetica(int Array[], int N){
+    int i,
+        Media;
+
+    for(Media=i=0; i<N; i++){
+        Media = Media + Array[i];
+    }
+    Media = Media/N;
+
+    return Media;
+}
